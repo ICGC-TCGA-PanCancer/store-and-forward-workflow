8ef7091058b1537f58f3f839cb312a3f228339f5
nbyrne <niall.byrne@oicr.on.ca>
2015-08-06 14:42:01 -0500

    .

--- workflow/scripts/timing.py
+++ workflow/scripts/timing.py
@@ -7,7 +7,7 @@ data['download'] = "download_timing.txt"
 data['upload'] = "upload_timing.txt"
 data['workflow'] = "workflow_timing.txt"
 
-tags = ['start', 'stop', 'delta']
+tags = ['start', 'stop', 'delta', 'size']
 
 start = {}
 stop = {}
@@ -21,6 +21,12 @@ def main(uuid):
 		stop[key] = float(timings[1])
 		delta[key] = float(timings[1]) - float(timings[0])
 
+	with open("download.size") as f:
+		download_size = f.read()
+
+	with open("upload.size") as f:
+		upload_size = f.read()
+
 	with open("%s.timing" % uuid,"w") as f:
 		iterable = list(data.keys())
 		sorted(iterable)
@@ -28,10 +34,10 @@ def main(uuid):
 		for i in iterable:
 			for tag in tags:
 				string += "%s-%s," % (i, tag)
-		f.write(string + "\n")
+		f.write(string + "download-size,upload-size,\n")
 		string = uuid + ","
 		for key in iterable:
-			string += "%s,%s,%s," % (start[key], stop[key], delta[key])
+			string += "%s,%s,%s,%s,%s," % (start[key], stop[key], delta[key], download_size, upload_size)
 		f.write(string + "\n")
 
 if __name__ == '__main__':
