bcd2ea1f1295c6d28537516ab24b8ac8bf241973
nbyrne <niall.byrne@oicr.on.ca>
2015-08-13 11:35:08 -0500

    .

--- src/main/java/io/seqware/pancancer/StoreAndForward.java
+++ src/main/java/io/seqware/pancancer/StoreAndForward.java
@@ -328,7 +328,6 @@ public class StoreAndForward extends AbstractWorkflowDataModel {
     			  );
     	  S3job.getCommand().addArgument("fail=$? \n");
     	  S3job.getCommand().addArgument("echo \"Received a $fail exit code from the upload container.\" \n");
-    	  S3job.getCommand().addArgument("cd " + this.analysisIds.get(index) + " \n");
     	  S3job.getCommand().addArgument("for x in logs/*; do sudo mv $x \"logs/" + this.analysisIds.get(index) + "_$(date +%s | tr -d '\\n')_$(basename $x | tr -d '\\n')\"; done \n");
     	  S3job.getCommand().addArgument("docker run "
     			  + "-v `pwd`:/collab/upload "
@@ -336,7 +335,6 @@ public class StoreAndForward extends AbstractWorkflowDataModel {
     			  + " bash -c \"s3cmd put /collab/upload/logs/* " + this.collabLogBucket + " --secret_key=" + this.collabLogSecret + " --access_key=" + this.collabLogKey + "\" \n"
     			  );
     	  // S3job.getCommand().addArgument("sudo mv logs ../logs.uploaded \n");
-    	  S3job.getCommand().addArgument("cd .. \n");
     	  index += 1;
       }
       S3job.getCommand().addArgument("du -c . | grep total | awk '{ print $1 }' > ../upload.size \n");
