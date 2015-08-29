b79f1aaf100226020ae868ae143b289d9b78fc39
ICGC AUTOMATION <nbyrne.oicr@gmail.com>
2015-08-29 22:19:36 +0000

    Make atomic git steps for idompotent.

--- src/main/java/io/seqware/pancancer/StoreAndForward.java
+++ src/main/java/io/seqware/pancancer/StoreAndForward.java
@@ -185,14 +185,14 @@ public class StoreAndForward extends AbstractWorkflowDataModel {
     	manageGit.getCommand().addArgument("if [[ ! -d " + path + " ]]; then mkdir -p " + path + "; fi \n");
     	manageGit.getCommand().addArgument("cd " + path + " \n");
     	manageGit.getCommand().addArgument("# This is not idempotent: git pull \n");
-    	manageGit.getCommand().addArgument("git checkout master \n");
     	manageGit.getCommand().addArgument("git reset --hard origin/master \n");
-    	manageGit.getCommand().addArgument("git fetch --all \n");
+    	manageGit.getCommand().addArgument("git pull \n");
     	manageGit.getCommand().addArgument("if [[ ! -d " + dst + " ]]; then mkdir " + dst + "; fi \n");
     	manageGit.getCommand().addArgument("if [[ -d " + src + " ]]; then git mv " + path + "/" + src + "/" + this.JSONfileName + " " + path + "/" + dst + "; fi \n");
     	manageGit.getCommand().addArgument("git stage . \n");
     	manageGit.getCommand().addArgument("git commit -m '" + dst + ": " + this.JSONfileName +"' \n");
     	manageGit.getCommand().addArgument("git push \n");
+    	manageGit.getCommand().addArgument("sleep $(bc <<< \"scale=5; $RANDOM / 32767 * 10\"  ) \n");
     	manageGit.addParent(lastJob);
     	return(manageGit);
     }
